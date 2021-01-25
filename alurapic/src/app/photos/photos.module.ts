91c3bd7389959641012361419a7f0b62900fc95c
jmkinjo <71389590+jmkinjo@users.noreply.github.com>
2021-01-24 22:06:09 -0300

    erro-05 BrowserModule e CommonModule

---
+++ alurapic/src/app/photos/photos.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { PhotoComponent } from './photo/photo.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    declarations: [ PhotoComponent, PhotoListComponent ],
+    imports: [ HttpClientModule,
+               CommonModule
+            ]
+})
+export class PhotosModule {}
